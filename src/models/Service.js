774ee23a11657cbc04773400bbed04ec7d70e5df
rashidsarkar <rashidrock558@gmail.com>
2023-11-23 18:02:38 +0600

    ``` Add service_ID, service_Name_With_req.name, service_Name_NO_req.name, facility.name fields to the Service model. ``` (generated by blackbox.ai )

--- src/models/Service.js
+++ src/models/Service.js
@@ -2,6 +2,23 @@ const { Schema } = require("mongoose");
 const ServiceSchema = new Schema({
   title: {
     type: String,
+  },
+  service_ID: {
+    type: Number,
     required: true,
   },
+  service_Name_With_req: {
+    name: String,
+    required: true,
+  },
+  service_Name_NO_req: {
+    name: String,
+  },
+  facility: [
+    {
+      name: String,
+      required: true,
+    },
+  ],
 });
+const Service = mongoose.model("Service", ServiceSchema);
